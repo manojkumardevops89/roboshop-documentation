7078ea3c241f943413d02bcc67e680c3c71a7c3b
manojkumardevops89 <manojkumar3177arusam@gmail.com>
2026-03-23 23:31:28 +0530

    Create vpc.tf

---
+++ Terraform/vpc.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = element(["us-east-1a","us-east-1b"], count.index)
+}
