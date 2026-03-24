de60505d67be185245abfcd7da960c56ba354db0
manojkumardevops89 <manojkumar3177arusam@gmail.com>
2026-03-24 09:57:18 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,51 +1,71 @@
 variable "region" {
   description = "AWS region"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "cluster_name" {
   description = "EKS Cluster name"
+  type        = string
   default     = "roboshop-eks"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.29"
 }
 
 variable "vpc_cidr" {
   description = "VPC CIDR block"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
   description = "Public subnet CIDRs"
+  type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
   description = "Private subnet CIDRs"
+  type        = list(string)
   default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "availability_zones" {
-  description = "AZs"
+  description = "Availability zones"
+  type        = list(string)
   default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "node_instance_type" {
   description = "EKS node instance type"
+  type        = string
   default     = "t3.medium"
 }
 
 variable "desired_capacity" {
-  default = 2
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
 }
 
 variable "min_size" {
-  default = 1
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
 }
 
 variable "max_size" {
-  default = 3
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
 }
 
 variable "domain_name" {
-  description = "Route53 domain"
+  description = "Route53 domain for RoboShop"
+  type        = string
   default     = "roboshop.yourdomain.com"
 }
