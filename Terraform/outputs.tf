63a3c0d11a308fa796be8d21d38b39ccff74f257
manojkumardevops89 <manojkumar3177arusam@gmail.com>
2026-03-24 10:04:32 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,39 +1,11 @@
 output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
-}
-
-output "public_subnets" {
-  description = "Public Subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "private_subnets" {
-  description = "Private Subnets"
-  value       = module.vpc.private_subnets
+  value = aws_vpc.main.id
 }
 
 output "eks_cluster_name" {
-  description = "EKS Cluster Name"
-  value       = module.eks.cluster_name
+  value = aws_eks_cluster.main.name
 }
 
 output "eks_cluster_endpoint" {
-  description = "EKS API endpoint"
-  value       = module.eks.cluster_endpoint
-}
-
-output "eks_cluster_security_group_id" {
-  description = "Cluster security group"
-  value       = module.eks.cluster_security_group_id
-}
-
-output "node_group_arn" {
-  description = "Node group ARN"
-  value       = module.eks.eks_managed_node_groups
-}
-
-output "route53_record" {
-  description = "Application DNS"
-  value       = var.domain_name
+  value = aws_eks_cluster.main.endpoint
 }
